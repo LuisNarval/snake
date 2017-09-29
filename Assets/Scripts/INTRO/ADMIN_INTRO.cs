750937a59e638bb53b36cb95297c3c879c400e04
Jose <impel.up@gmail.com>
2017-09-29 07:07:56 -0500

    Diecinueveavo Commit. Se agrego la escena de Intro. La escena ya contiene su animación y su respectiva programación. Al terminar de ejecutarse manda al jugador al Menú Principal.

---
+++ Assets/Scripts/INTRO/ADMIN_INTRO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ADMIN_INTRO : MonoBehaviour {
+
+    public void irAMenu() {
+        FADER.instancia.siguienteEscena("Menu");
+    }
+
+}
