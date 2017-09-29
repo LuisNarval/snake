e327d423bec1791408a1cfa90f2f205c99fdd57c
Jose <impel.up@gmail.com>
2017-09-28 21:14:44 -0500

    Treceavo Commit. Se ha animado el menu de inicio y se ha conectado el script de ADMIN_MENU con sus respectivas animaciones. Queda pendiente el Fader para transición entre escenas y cerrar la aplicación.

---
+++ Assets/Scripts/ADMIN_MENU.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ADMIN_MENU : MonoBehaviour {
+
+    public Animator animadorMenu;
+
+    // Use this for initialization
+    void Start () {}
+    
+    // Update is called once per frame
+    void Update () {}
+
+    public void irAJuego()
+    {
+    }
+
+
+    public void irADificultad()
+    {
+        animadorMenu.SetTrigger("irADificultad");
+    }
+
+    public void irAPuntuaciones()
+    {
+        animadorMenu.SetTrigger("irAPuntuaciones");
+    }
+
+    public void salirDificultad()
+    {
+        animadorMenu.SetTrigger("salirDificultad");
+    }
+
+    public void salirPuntuaciones()
+    {
+        animadorMenu.SetTrigger("salirPuntuaciones");
+    }
+
+    public void salir()
+    {
+    }
+
+
+}
